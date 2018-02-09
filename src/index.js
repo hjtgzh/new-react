c53024eac0480353784a30852e3c501235bcec56
hjtgzh1 <huangjianting@thundersdata.com>
2018-02-09 17:09:03 +0800

    添加ant，路由

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { useStrict } from 'mobx';
+import { Provider } from 'mobx-react';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
+useStrict(true); // 开启mobx严格模式，强制要求所有属性的修改都要通过action的方式定义
---
+++ src/pages/home/Home.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+class Home extends React.PureComponent {
+  render() {
+    return <div className="">home</div>;
+  }
+}
+export default Home;
